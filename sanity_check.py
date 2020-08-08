f375e19a367d46652999152148aa92ea99486cbf
James Spears <james.w.spears@gmail.com>
2020-08-08 18:39:28 -0400

    add doc strings to sanity check

---
+++ sanity_check.py
@@ -0,0 +1,10 @@
+"""
+sanity_check.py - Module - The sanity_check.py module contains the SanityCheck class.
+"""
+
+
+class SanityCheck:
+    """
+    SanityCheck - Class - The SanityCheck class encapsulates logic related
+    to ensuring the completed build has resulted in a usable machine learning model.
+    """
